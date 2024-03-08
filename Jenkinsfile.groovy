c2a87a47f0b400590e237305b9d7d97a6c6e886d
Gagana <92525934+gaganakg@users.noreply.github.com>
2024-03-08 16:08:32 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                build 'PES2UG21CS173-1'
+                sh 'g++ pipeline_test_task5.cpp -o output' 
+            }
+        }
+        stage('Test') {
+            steps {
+                sh './output' 
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying...' 
+            }
+        }
+    }
+    post {
+        failure {
+            error 'Pipeline failed'
+        }
+    }
+}
